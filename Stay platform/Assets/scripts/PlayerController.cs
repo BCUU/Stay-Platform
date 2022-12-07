ea9ab01f9473e650907ef019558f11f08c9283ea
berat can uçar <canberat__@outlook.com>
2022-12-07 19:28:34 +0300

    Camera and Player
    Created "rotatecamera" and "playercontroller" scripts can now move the game to the focal point

---
+++ Stay platform/Assets/scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private GameObject focalPoint;
+
+    private Rigidbody playerRb;
+
+    public bool hasPowerUp = false;
+
+    private float powerupstrength = 15f;
+    public GameObject selectionRings;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        selectionRings.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("powerup"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            selectionRings.SetActive(true);
+            StartCoroutine(PowerupCcountdownRoutine());
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("enemy") && hasPowerUp == true)
+        {
+            Rigidbody enemyRigidb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRigidb.AddForce(awayFromPlayer * powerupstrength, ForceMode.Impulse);
+        }
+    }
+    IEnumerator PowerupCcountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        selectionRings.SetActive(false);
+
+    }
+}
---
+++ Stay platform/Assets/scripts/RoteCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoteCamera : MonoBehaviour
+{
+    public float RotationSpeed;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * RotationSpeed * Time.deltaTime);
+    }
+}
